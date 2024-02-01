7daad91ece2bcecf3bf64e126a6811f24216fa86
Fomin Alexey <fomin.alexey@outlook.com>
2024-02-01 21:17:31 +0300

    *

--- 02-Seminars/Homework02/src/main/drugstore/Component.java
+++ 02-Seminars/Homework02/src/main/drugstore/Component.java
@@ -1,6 +1,6 @@
 package main.drugstore;
 
-public abstract class Component {
+public abstract class Component implements Comparable<Component> {
     private String title;
     private Double weight;
     private int power;
@@ -14,6 +14,21 @@ public abstract class Component {
     @Override
     public String toString() {
         return String.format("Title = %s, power = %s", title, power);
+    }
+
+    @Override
+    public int compareTo(Component o) {
+//  return this.power - o.power;
+        return Integer.compare(this.power, o.power);
+
+//    if(this.power < o.power){
+//        return -1;
+//    }
+//    else if (this.power > o.power){
+//        return 1;
+//    }
+//    else return 0;
+
     }
 }
 
