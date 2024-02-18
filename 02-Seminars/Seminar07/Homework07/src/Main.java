0f63250d8a14a9cc4e32dceefd35e6eb5b4791dd
Fomin Alexey <fomin.alexey@outlook.com>
2024-02-18 21:32:44 +0300

    readme

--- 02-Seminars/Seminar07/Homework07/src/Main.java
+++ 02-Seminars/Seminar07/Homework07/src/Main.java
@@ -4,7 +4,6 @@ import ComplexCalc.ComplexNumber;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
         Calculator calculator = new Calculator();
         System.out.println(
                 calculator.calculate(10, '*', 20)
